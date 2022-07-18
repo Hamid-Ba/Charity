72378ef68764fa725eda39addb0aa094544414fc
hamidba <balalzadehhamid79@gmail.com>
2022-07-18 16:02:21 +0430

    Complete App Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,28 @@
+from tokenize import blank_re
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    GENDER = (
+        ('M','male'),
+        ('F','female'),
+    )
+    
+    address = models.TextField(blank=True)
+    age = models.PositiveSmallIntegerField(blank=True)
+    date_joined = models.DateTimeField()
+    description = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    first_name = models.CharField(blank=True)
+    last_name = models.CharField(blank=True)
+    gender = models.CharField(choices=GENDER,max_length=1,blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    last_login = models.DateTimeField(blank=True)
+    password = models.CharField()
+    phone = models.CharField(max_length=15,blank=True)
+    username = models.CharField()
+
+
--- charities/models.py
+++ charities/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
+from accounts.models import User
+
 
 class Benefactor(models.Model):
-    pass
+    user = models.OneToOneField(User)
+    experience = models.SmallIntegerField(choices=(0,1,2),default=0)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
 
 
 class Task(models.Model):
