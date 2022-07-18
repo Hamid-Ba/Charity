57fb86074ae970eefb84ce68b6c5a0ab63f1ede5
hamidba <balalzadehhamid79@gmail.com>
2022-07-18 17:10:12 +0430

    Change Models Fields

--- charities/models.py
+++ charities/models.py
@@ -4,13 +4,19 @@ from accounts.models import User
 
 
 class Benefactor(models.Model):
-    user = models.OneToOneField(User)
-    experience = models.SmallIntegerField(choices=(0,1,2),default=0)
+    EXPERIENCE = (
+        (0,'beginner'),
+        (1,'intermediate'),
+        (2,'advance'),  
+    )
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(choices=EXPERIENCE,default=0)
     free_time_per_week = models.PositiveSmallIntegerField(default=0)
 
 
 class Charity(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     reg_number = models.CharField(max_length=10)
 
@@ -28,12 +34,12 @@ class Task(models.Model):
         ('D','Done'),
     )
 
-    assigned_benefactor = models.ForeignKey(Benefactor, nullable=True,on_delete=models.SET_NULL)
-    charity = models.ForeignKey(Charity)
-    age_limit_from = models.IntegerField()
-    age_limit_to = models.IntegerField()
-    date = models.DateField()
-    description = models.TextField()
-    gender_limit = models.CharField(choices=GENDER,max_length=1)
+    assigned_benefactor = models.ForeignKey(Benefactor, null=True,on_delete=models.SET_NULL)
+    charity = models.ForeignKey(Charity,null=True,on_delete=models.SET_NULL)
+    age_limit_from = models.IntegerField(blank=True,null=True)
+    age_limit_to = models.IntegerField(blank=True,null=True)
+    date = models.DateField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    gender_limit = models.CharField(choices=GENDER,max_length=1,blank=True,null=True)
     state = models.CharField(choices=STATE,default='P',max_length=1)
     title = models.CharField(max_length=60)
